1e20105b4abbe1380ff886504bb72d20966237e9
Jesse Hopkins <jesse.hopkinsm@gmail.com>
2018-01-17 16:33:01 -0600

    Added template: index to Templates>Workout

--- src/main/java/launchcode/org/ExerciseTracker/Controllers/WorkoutController.java
+++ src/main/java/launchcode/org/ExerciseTracker/Controllers/WorkoutController.java
@@ -1,9 +1,17 @@
 package launchcode.org.ExerciseTracker.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "Workout")
+@RequestMapping(value = "workout")
 public class WorkoutController {
+
+    @RequestMapping(value="")
+    public String index(Model model) {
+
+      model.addAttribute("title", "Workout Session");
+      return "workout/index";
+    }
 }
