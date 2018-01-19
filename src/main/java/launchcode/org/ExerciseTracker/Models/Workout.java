c82b96c8be3934c9943ee8a6a526dbfc610f6f75
Jesse Hopkins <jesse.hopkinsm@gmail.com>
2018-01-19 17:03:08 -0600

    Added initial getters and setters to Workout class.

--- src/main/java/launchcode/org/ExerciseTracker/Models/Workout.java
+++ src/main/java/launchcode/org/ExerciseTracker/Models/Workout.java
@@ -1,7 +1,26 @@
 package launchcode.org.ExerciseTracker.Models;
 
-public class Workout {
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.util.Date; //use bootstrap date picker
+
+public class Workout {
     private String name;
-    private String Date;
+    private java.util.Date Date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public java.util.Date getDate() {
+        return Date;
+    }
+
+    public void setDate(java.util.Date date) {
+        Date = date;
+    }
 }
