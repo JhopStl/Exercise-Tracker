15b49c48b1ae322ad3e1a96897d2643c0ce4bc30
Jesse Hopkins <jesse.hopkinsm@gmail.com>
2018-01-23 23:24:37 -0600

    Added JPA dependencies.

--- src/main/java/launchcode/org/ExerciseTracker/Controllers/WorkoutController.java
+++ src/main/java/launchcode/org/ExerciseTracker/Controllers/WorkoutController.java
@@ -1,4 +1,4 @@
-package launchcode.org.ExerciseTracker.Controllers;
+package launchcode.org.ExerciseTracker.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,10 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "workout")
 public class WorkoutController {
 
+
+    //handler to list workout sessions (times went to the gym)
     @RequestMapping(value="")
     public String index(Model model) {
 
       model.addAttribute("title", "Workout Session");
       return "workout/index";
+
+    }
+
+    //handler to display add workout form
+    @RequestMapping(value="add")
+    public String addWorkout(Model model) {
+
+        model.addAttribute("title", "Add Workout");
+        return "workout/add";
     }
 }
--- src/main/java/launchcode/org/ExerciseTracker/Models/Workout.java
+++ src/main/java/launchcode/org/ExerciseTracker/Models/Workout.java
@@ -1,12 +1,15 @@
-package launchcode.org.ExerciseTracker.Models;
+package launchcode.org.ExerciseTracker.models;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Entity;
 import java.util.Date; //use bootstrap date picker
 
+@Entity
 public class Workout {
     private String name;
     private java.util.Date Date;
+    private int id;
 
     public String getName() {
         return name;
