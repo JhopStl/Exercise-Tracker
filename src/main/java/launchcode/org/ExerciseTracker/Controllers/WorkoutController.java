a1e2e7d596c618324dae04c55627a110992cbe6a
Jesse Hopkins <jesse.hopkinsm@gmail.com>
2018-02-06 22:39:53 -0600

    Added table to workout view, added workoutDao.save(), application now saves ID to DB but not date and name

--- src/main/java/launchcode/org/ExerciseTracker/Controllers/WorkoutController.java
+++ src/main/java/launchcode/org/ExerciseTracker/Controllers/WorkoutController.java
@@ -1,10 +1,13 @@
 package launchcode.org.ExerciseTracker.controllers;
 
+import launchcode.org.ExerciseTracker.models.Workout;
 import launchcode.org.ExerciseTracker.models.data.WorkoutDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "workout")
@@ -15,6 +18,7 @@ public class WorkoutController {
     private WorkoutDao workoutDao;
 
     //handler to list workout sessions (times went to the gym)
+    //links view to Workoutdao
     @RequestMapping(value="")
     public String index(Model model) {
         model.addAttribute("workouts", workoutDao.findAll());
@@ -24,10 +28,22 @@ public class WorkoutController {
     }
 
     //handler to display add workout form
-    @RequestMapping(value="add")
+    //get request
+    @RequestMapping(value="add", method = RequestMethod.GET)
     public String addWorkout(Model model) {
 
         model.addAttribute("title", "Add Workout");
+        model.addAttribute(new Workout());
         return "workout/add";
+    }
+
+    @RequestMapping(value="add", method = RequestMethod.POST)
+    public String processWorkout(@ModelAttribute Workout newWorkout, Model model) {
+
+        model.addAttribute("title", "Add Workout");
+
+        //save workout to DB
+        workoutDao.save(newWorkout);
+        return "redirect:";
     }
 }
