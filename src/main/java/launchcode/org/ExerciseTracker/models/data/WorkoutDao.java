f0428f1bafc49ba9bebfe6c9eedd5caa1c4bb9c0
Jesse Hopkins <jesse.hopkinsm@gmail.com>
2018-02-16 16:25:59 -0600

    updated comments

--- src/main/java/launchcode/org/ExerciseTracker/models/data/WorkoutDao.java
+++ src/main/java/launchcode/org/ExerciseTracker/models/data/WorkoutDao.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
-//Data Access Object - Interface that allows us to access cheese classes and interact with the DB
+//Data Access Object - Interface that allows us to access cheese classes and interact with the DB.  This class touches the DB, we don't.
 //crud repository class specifies methods that will allow us to send and retrieve data from DB
 
 @Repository
-@Transactional
+@Transactional //all methods should be wrapped into one transaction
 public interface WorkoutDao extends CrudRepository<Workout,Integer> {
 }
