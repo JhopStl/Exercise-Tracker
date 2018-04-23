68b9b97d65637ac15e1b9f56ad3cabb2c98ebc1f
Jesse Hopkins <jesse.hopkinsm@gmail.com>
2018-04-22 22:32:01 -0500

    Began adding fields to user class

--- src/main/java/launchcode/org/ExerciseTracker/models/User.java
+++ src/main/java/launchcode/org/ExerciseTracker/models/User.java
@@ -1,4 +1,27 @@
 package launchcode.org.ExerciseTracker.models;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+
+    private int id;
+    @NotEmpty(message = "*Username is required")
+    private String userName;
+    @Email(message = "*Please provide a valid Email")
+    @NotEmpty(message = "*Email is required")
+    private String email;
+
+    @NotEmpty(message = "*Please provide a password")
+    @Transient
+    private String password;
 }
