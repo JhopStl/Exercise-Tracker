bd63d3c9cb575b69b69dc24609097edf38a88e1e
Jesse Hopkins <jesse.hopkinsm@gmail.com>
2018-04-29 16:19:42 -0500

    Forced exclusion of securityConfig class

--- src/main/java/launchcode/org/ExerciseTracker/ExerciseTrackerApplication.java
+++ src/main/java/launchcode/org/ExerciseTracker/ExerciseTrackerApplication.java
@@ -2,8 +2,9 @@ package launchcode.org.ExerciseTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class ExerciseTrackerApplication {
 
 	public static void main(String[] args) {
