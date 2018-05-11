edf1284a1f77a4537d0d722c57eb00e89f25c53f
Jesse Hopkins <jesse.hopkinsm@gmail.com>
2018-05-11 12:25:38 -0500

    Changed path of login and registration template

--- src/main/java/launchcode/org/ExerciseTracker/controllers/LoginController.java
+++ src/main/java/launchcode/org/ExerciseTracker/controllers/LoginController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,10 +25,10 @@ public class LoginController {
     @Autowired
     private RoleDao roleDao;
 
-    @RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+    @RequestMapping(value={"/", "/Templates/login"}, method = RequestMethod.GET)
     public ModelAndView login(){
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("login/login");
+        modelAndView.setViewName("Templates/login/Templates.login");
         return modelAndView;
     }
 
@@ -43,7 +42,7 @@ public class LoginController {
 
         User user = new User();
         modelAndView.addObject("user", user);
-        modelAndView.setViewName("login/registration");
+        modelAndView.setViewName("Templates/login/registration");
         return modelAndView;
     }
 
@@ -57,12 +56,12 @@ public class LoginController {
                             "There is already a user registered with the email provided");
         }
         if (bindingResult.hasErrors()) {
-            modelAndView.setViewName("login/registration");
+            modelAndView.setViewName("Templates/login/registration");
         } else {
             userService.saveUser(user);
             modelAndView.addObject("successMessage", "User has been registered successfully");
             modelAndView.addObject("user", new User());
-            modelAndView.setViewName("login/registration");
+            modelAndView.setViewName("Templates/login/registration");
 
         }
         return modelAndView;
