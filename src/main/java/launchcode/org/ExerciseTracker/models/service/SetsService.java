69cef14d96af49c9a2d41d8d3bd84023e44e8379
Jesse Hopkins <jesse.hopkinsm@gmail.com>
2018-07-20 16:44:14 -0500

    Added SetsService, need to add an implementation class

---
+++ src/main/java/launchcode/org/ExerciseTracker/models/service/SetsService.java
@@ -0,0 +1,12 @@
+package launchcode.org.ExerciseTracker.models.service;
+
+import launchcode.org.ExerciseTracker.dto.SetsDTO;
+import launchcode.org.ExerciseTracker.models.Sets;
+
+import java.util.List;
+
+public interface SetsService {
+    List<Sets> findSetsByExerciseId(int exerciseId);
+    Sets addSets(SetsDTO setsDTO);
+    Sets findSetsById(int id);
+}
